bd5c896e428be6f128a8dd84db0ac0b6b5ff6a14
tirthdoshi009 <tirthdoshi009@gmail.com>
2020-05-28 16:38:53 -0400

    forward pass completed

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
 var cols, rows;
 var w = 40;
 var grid = [];
+
+var current ;
+
+
 function setup(){
     createCanvas(400,400);
     cols = floor(width/w);
     rows = floor(height/w);
+    frameRate(5);
     for(var j = 0;j<rows;j++){
-        for(var i = 0;i<cols;i++){
+        for(var i = 0;i<cols;i++){ 
             var cell = new Cell(i,j);
             grid.push(cell);
         }
     }
+    current = grid[0];
+}
+
+function index(i, j){
+    //Count for the edge cases
+    if(i<0 || j<0 || i>cols-1 || j>rows-1){
+        return -1;
+    }
+    return i+j *cols;
 }
 
 function draw(){
@@ -18,18 +32,54 @@ function draw(){
     for(var i=0;i< grid.length;i++){
         grid[i].show();
     }
+    current.visited = true;
+    var next = current.checkNeighbors();
+    if(next){
+        next.visited = true;
+        current = next;
+    }
 }
 
 function Cell(i,j){
     this.i = i;
     this.j = j;
-    this.walls = [true, true, true, true ]
+    this.walls = [true, true, true, true ];
+    this.visited = false;
+    this.checkNeighbors = function(){
+        var neighbors = [];
+        var top = grid[index(i,j-1)];
+        var right = grid[index(i+1,j)];
+        var bottom = grid[index(i,j+1)];
+        var left = grid[index(i-1,j)];
+
+        if(top && !top.visited){
+            neighbors.push(top);
+        }
+        if(right && !right.visited){
+            neighbors.push(right);
+        }
+        if(bottom && !bottom.visited){
+            neighbors.push(bottom);
+        }
+        if(left && !left.visited){
+            neighbors.push(left);
+        }
+
+        if(neighbors.length>0){
+            var r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        }
+        else{
+            return undefined;
+        }
+
+    }
     this.show = function(){
         var x = this.i*w;
         var y = this.j*w;
         stroke(255);
         // noFill(); 
-        // rect(x,y,w,w);
+        
         if(this.walls[0]){
             line(x   , y   , x+w ,y);
         }
@@ -41,6 +91,10 @@ function Cell(i,j){
         }
         if(this.walls[3]){
             line(x   , y+w , x   ,y);
+        }
+        if(this.visited){
+            fill(255,0,255,100);
+            rect(x,y,w,w);
         }
     }
 }
